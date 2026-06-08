077e199fd3a5001d963ab8e5d9f98cf64ffdbc54
Kazuma Arino <hogeika2@gmail.com>
2026-06-08 22:23:39 +0900

    Ver 4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.karino2.magnasketch"
         minSdk = 28
         targetSdk = 37
-        versionCode = 3
-        versionName = "3"
+        versionCode = 4
+        versionName = "4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
